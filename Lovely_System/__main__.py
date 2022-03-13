2aa3788709a91da51401cdeba4e57577a83e9ffc
gprose1234gmail <95291555+gprose1234gmail@users.noreply.github.com>
2022-03-13 15:11:32 +0530

    Update __main__.py

--- Lovely_System/__main__.py
+++ Lovely_System/__main__.py
@@ -43,7 +43,7 @@ for load in to_load:
 
 @System.on(system_cmd(pattern=r"status", allow_enforcer=True))
 async def status(event):
-    msg = await event.reply("Connecting to Lovely Systems.")
+    msg = await event.reply("Connecting to 𝙻𝚄𝙽𝙰 𝚇 𝚂𝚈𝚂𝚃𝙴𝙼.")
     time.sleep(1)
     await msg.edit("Initialising ■□□□□□")
     time.sleep(1)
