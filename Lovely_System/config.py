efaae0cf35174c2f7500a7f92a9d136bd29b01e6
gprose1234gmail <95291555+gprose1234gmail@users.noreply.github.com>
2022-03-13 15:27:56 +0530

    Update config.py

--- Lovely_System/config.py
+++ Lovely_System/config.py
@@ -3,11 +3,11 @@ from typing import Union, Optional
 API_ID: int = 12345
 API_HASH: str = "XYZ"
 STRING_SESSION: str = "XYZ:XYZ:XYZ...."
-Lovely_logs: Union[str, int] = -1001512548429
-Lovely_approved_logs: Union[str, int] = -1001476531091
+Lovely_logs: Union[str, int] = -1001635517276
+Lovely_approved_logs: Union[str, int] = -1001615945740
 GBAN_MSG_LOGS: Optional[Union[str, int]] = None
 BOT_TOKEN: str = "123456:shsiajskzkxhxj"
-ENFORCERS: int = 123488
-INSPECTORS: int = 1993883
-MONGO_DB_URL: str = "1993838:sjsh"
+ENFORCERS: int = 1930954213
+INSPECTORS: int = 1930954213
+MONGO_DB_URL: str = "mongodb+srv://shikhar:shikhar@cluster0.6xzlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 # Creadits gose to https://github.com/Hodacka
